1ad1d509a8af0b1f0f525ec0702bcf6457c8ee74
Romanchik Alkesey <romanchikaleksey@gmail.com>
2022-03-31 20:43:47 +0300

    добавление методов регистрации сервисов

--- FriendlyDIContainer/ServiceProvider.cs
+++ FriendlyDIContainer/ServiceProvider.cs
@@ -2,5 +2,41 @@
 {
     public class ServiceProvider
     {
+        private readonly Dictionary<Type, Service> _services = new Dictionary<Type, Service>();
+
+        #region public methods
+
+        public void RegisterService<TInterfaceType, TInstanceType>(Func<object>? implementationFactory = null)  
+            where TInstanceType : class, TInterfaceType
+        {
+            RegisterService<TInterfaceType, TInstanceType>(implementationFactory);
+        }
+
+        public void RegisterService<TInstanceType>(Func<object>? implementationFactory = null)
+            where TInstanceType : class
+        {
+            RegisterService<TInstanceType, TInstanceType>(implementationFactory);
+        }
+
+        public void RegisterSingletonService<TInstanceType>(Func<object>? implementationFactory = null)
+            where TInstanceType : class
+        {
+            RegisterService<TInstanceType, TInstanceType>(true, implementationFactory);
+        }
+
+        public void RegisterSingletonService<TInterfaceType, TInstanceType>(Func<object>? implementationFactory = null)
+            where TInstanceType : class, TInterfaceType
+        {
+            RegisterService<TInstanceType, TInstanceType>(true, implementationFactory);
+        }
+
+        #endregion public methods
+
+        private void RegisterService<TInterfaceType, TInstanceType>(bool isSingleton = false,
+            Func<object>? implementationFactory = null) where TInstanceType : class, TInterfaceType
+        {
+            _services.Add(typeof(TInterfaceType),
+                          new Service(typeof(TInterfaceType), isSingleton, implementationFactory));
+        }
     }
 }
